5df826eb82ee30552bcbad0c5dc48edf9d3312f8
zatikhair <zati.khair@gmail.com>
2021-08-05 23:23:47 +0800

    update

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+function repeatString(n, str) {
+  let output = "";
+  for (let i = 0; i < n; i++) {
+    output += str;
+  }
+  return output;
+}
 
-};
+console.log(repeatString(3, "hello"));
 
 module.exports = repeatString;
