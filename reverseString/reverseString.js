7c9e97e5b908674cd1d04d64aa17410c21ce33c5
zatikhair <zati.khair@gmail.com>
2021-08-05 22:44:51 +0800

    update

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,12 +1,5 @@
-function reverseString(str) {
-  var words = [];
-  words = str.match(/\S+/g);
-  var result = "";
-  for (var i = 0; i < words.length; i++) {
-    result += words[i].split("").reverse().join("") + " ";
-  }
-  return result;
-}
-console.log(reverseString("abd fhe kdj"));
+const reverseString = (str) => [...str].reverse().join("");
+
+console.log(reverseString("ABC"));
 
 module.exports = reverseString;
