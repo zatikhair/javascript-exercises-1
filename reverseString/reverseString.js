be5460efedf7326f6bdab644067316cb7dec07b4
zatikhair <zati.khair@gmail.com>
2021-08-05 22:29:35 +0800

    update

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,21 +1,12 @@
 function reverseString(str) {
-  // return a new array of strings
-  const arrayStrings = str.split("");
-
-  // reverse the new created array elements
-  const reverseArray = arrayStrings.reverse();
-
-  // join all elements of the array into a string
-  const joinArray = reverseArray.join("");
-
-  // return the reversed string
-  return joinArray;
-}
-
-// take input from the user
-const string = console.log("Enter a string: ");
-
-const result = reverseString(string);
-console.log(result);
+    var words = [];
+    words = str.match(/\S+/g);
+    var result = "";
+    for (var i = 0; i < words.length; i++) {
+       result += words[i].split('').reverse().join('') + " ";
+    }
+    return result
+  }
+  console.log(reverseInPlace("abd fhe kdj")
 
 module.exports = reverseString;
