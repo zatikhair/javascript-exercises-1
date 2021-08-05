0637c68772f70ef6040018ff988385dd30d2c0fc
zatikhair <zati.khair@gmail.com>
2021-08-05 22:32:48 +0800

    update

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,12 +1,12 @@
 function reverseString(str) {
-    var words = [];
-    words = str.match(/\S+/g);
-    var result = "";
-    for (var i = 0; i < words.length; i++) {
-       result += words[i].split('').reverse().join('') + " ";
-    }
-    return result
+  var words = [];
+  words = str.match(/\S+/g);
+  var result = "";
+  for (var i = 0; i < words.length; i++) {
+    result += words[i].split("").reverse().join("") + " ";
   }
-  console.log(reverseInPlace("abd fhe kdj")
+  return result;
+}
+console.log(reverseString("abd fhe kdj"));
 
 module.exports = reverseString;
