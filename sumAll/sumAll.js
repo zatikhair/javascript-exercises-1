61ec106b46dea67922e74556ad849c642b5c693c
zatikhair <zati.khair@gmail.com>
2021-08-06 00:21:03 +0800

    update

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,31 +1,19 @@
-function sumAll(str) {
-  // A temporary string
-  let temp = "0";
-
-  // holds sum of all numbers
-  // present in the string
+function sumAll(arr) {
+  let fullArr = [];
   let sum = 0;
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-  // read each character in input string
-  for (let i = 0; i < str.length; i++) {
-    let ch = str[i];
+  arr.sort(function (a, b) {
+    return a - b;
+  });
 
-    // if current character is a digit
-    if (!isNaN(String(ch) * 1)) temp += ch;
-    // if current character is an alphabet
-    else {
-      // increment sum by number found earlier
-      // (if any)
-      sum += parseInt(temp);
-
-      // reset temporary string to empty
-      temp = "0";
-    }
+  for (let i = arr[0]; i <= arr[1]; i++) {
+    fullArr.push(i);
   }
 
-  // atoi(temp.c_str()) takes care of trailing
-  // numbers
-  return sum + parseInt(temp);
+  sum = fullArr.reduce(reducer);
+
+  return sum;
 }
 
 console.log(sumAll(1, 10));
