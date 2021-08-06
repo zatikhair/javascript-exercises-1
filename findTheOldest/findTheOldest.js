e19fc5441bfcc3db5c3615adc9e3454387c9bd0a
zatikhair <zati.khair@gmail.com>
2021-08-07 06:57:26 +0800

    update

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = people.reduce((prevVal, curVal) => {
+  return prevVal.yearOfDeath - prevVal.yearOfBirth <
+    curVal.yearOfDeath - curVal.yearOfBirth
+    ? curVal
+    : prevVal;
+});
+console.log(res.name, res.yearOfDeath - res.yearOfBirth);
 
 module.exports = findTheOldest;
