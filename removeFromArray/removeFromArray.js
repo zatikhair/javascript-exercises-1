8918de4c864be812d28f6a509f260f8e71340822
zatikhair <zati.khair@gmail.com>
2021-08-07 06:40:36 +0800

    update

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,7 +1,10 @@
-const removeFromArray = [0, 1, 2, 3, 4];
+const nums = [1, 2, 3, 4, 5, 6];
+const remove = [1, 2, 4, 6];
 
-[1, 4, 3].reverse().forEach((index) => {
-  items.splice(index, 1);
-});
+function removeFromArray(original, remove) {
+  return original.filter((value) => !remove.includes(value));
+}
+
+console.log(removeFromArray(nums, remove));
 
 module.exports = removeFromArray;
