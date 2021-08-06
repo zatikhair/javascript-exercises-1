db84dd930f8dffb8ebdd511d71d5ab86c3d6f948
zatikhair <zati.khair@gmail.com>
2021-08-07 06:43:34 +0800

    update

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,10 +1,5 @@
-const nums = [1, 2, 3, 4, 5, 6];
-const remove = [1, 2, 4, 6];
-
-function removeFromArray(original, remove) {
-  return original.filter((value) => !remove.includes(value));
-}
-
-console.log(removeFromArray(nums, remove));
+var removeFromArray = function (array, ...numbers) {
+  return array.filter((num) => ![...numbers].includes(num));
+};
 
 module.exports = removeFromArray;
