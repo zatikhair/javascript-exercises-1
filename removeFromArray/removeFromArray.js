57bcfa03c96476f08151c2c199259720a8f3eac6
zatikhair <zati.khair@gmail.com>
2021-08-07 05:57:38 +0800

    remove test

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = (array, itemToRemove) => {
+  return array.filter((v) => {
+    return !itemsToRemove.includes(v);
+  });
 };
+
+removeFromArray([1, 2, 3, 4, 5], [1, 4]);
 
 module.exports = removeFromArray;
