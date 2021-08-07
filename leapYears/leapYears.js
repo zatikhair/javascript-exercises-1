0e07e0c9f5a2095010870d75bc933d6de4d3cafd
zatikhair <zati.khair@gmail.com>
2021-08-07 22:52:54 +0800

    update

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let divisibleBy100 = year % 100 == 0;
+  let divisibleBy400 = year % 400 == 0;
 
+  if (year % 4 == 0) {
+    if (!divisibleBy100 || (divisibleBy100 && divisibleBy400)) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
 module.exports = leapYears;
