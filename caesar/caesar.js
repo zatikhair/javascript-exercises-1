b15ccf47715e6bae8c709f439b1eadf5c807bcd1
zatikhair <zati.khair@gmail.com>
2021-08-07 23:35:57 +0800

    update

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,24 @@
-const caesar = function() {
+const caesar = function (string, offset) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  let encodedArray = [];
+  let stringArray = string.split("");
+  stringArray.forEach(function (char, i) {
+    if (/[^a-zA-Z]/.test(char)) {
+      encodedArray.push(char);
+      return;
+    }
 
+    let isUpcase = char == char.toUpperCase();
+    let index = alphabet.indexOf(char.toLowerCase());
+    let shiftedChar = alphabet[(((index + offset) % 26) + 26) % 26];
+
+    if (isUpcase) {
+      encodedArray.push(shiftedChar.toUpperCase());
+    } else {
+      encodedArray.push(shiftedChar);
+    }
+  });
+  return encodedArray.join("");
 };
 
 module.exports = caesar;
