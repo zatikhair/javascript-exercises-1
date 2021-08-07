633308761517a8d7d17e1fdf4638daf83402a1e7
zatikhair <zati.khair@gmail.com>
2021-08-07 22:39:33 +0800

    update

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+var palindromes = function (string) {
+  var str = string.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  var reverse = str.split("").reverse().join("");
+  return str == reverse ? true : false;
 };
 
 module.exports = palindromes;
