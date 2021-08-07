ce95f0bb3106060a67a3d7910584be691c1d5d79
zatikhair <zati.khair@gmail.com>
2021-08-07 21:44:52 +0800

    update

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function (fahren) {
+  let celsi = (fahren - 32) * (5 / 9);
+  if (celsi % 1 == 0) return celsi;
+  return Math.round(celsi * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (celsi) {
+  let fahren = celsi * (9 / 5) + 32;
+  if (fahren % 1 == 0) return fahren;
+  return Math.round(fahren * 10) / 10;
 };
 
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
