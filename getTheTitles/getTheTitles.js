2efe0815264c220acf9212935f91719d53e48f7c
zatikhair <zati.khair@gmail.com>
2021-08-07 23:02:19 +0800

    update

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  let titles = [];
 
+  for (let i = 0; i < books.length; i++) {
+    titles.push(books[i].title);
+  }
+
+  return titles;
 };
 
 module.exports = getTheTitles;
