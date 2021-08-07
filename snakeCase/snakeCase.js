dcae809a664aab7fdeb0e289a5e13f62755521ce
zatikhair <zati.khair@gmail.com>
2021-08-07 21:58:04 +0800

    update

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,18 @@
-const snakeCase = function() {
+const snakeCase = function (str) {
+  str = str.split("..").join(" ");
+  str = str
+    .toLowerCase()
+    .replace(/[^\w\s-]|_/g, "")
+    .replace(/[\s-]+/g, "_");
 
+  // to remove trailing '_'
+  let lastChar = str[str.length - 1];
+  while (lastChar == "_") {
+    str = str.substring(0, str.length - 1);
+    lastChar = str[str.length - 1];
+  }
+
+  return str;
 };
 
 module.exports = snakeCase;
