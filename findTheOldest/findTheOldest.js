c4940c89278a9bb306e681b77c28b0f128e062fb
zatikhair <zati.khair@gmail.com>
2021-08-07 21:18:18 +0800

    update

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,7 +1,21 @@
-const findTheOldest = characters.sort(function (a, b) {
-  const lastPerson = a.death - a.birthdate;
-  const nextPerson = b.death - b.birthdate;
-  return lastPerson > nextPerson ? -1 : 1;
-});
+let findTheOldest = function (people) {
+  let oldest = 0;
+
+  for (let i = 0; i < people.length; i++) {
+    // make sure still-living person's current ages are properly computed
+    if (people[i].yearOfDeath == undefined) {
+      people[i].yearOfDeath = new Date().getFullYear();
+    }
+
+    let personsAge = people[i].yearOfDeath - people[i].yearOfBirth;
+    let oldestsAge = people[oldest].yearOfDeath - people[oldest].yearOfBirth;
+
+    if (personsAge > oldestsAge) {
+      oldest = i;
+    }
+  }
+
+  return people[oldest];
+};
 
 module.exports = findTheOldest;
