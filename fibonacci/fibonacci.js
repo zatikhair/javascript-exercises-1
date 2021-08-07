76cb228988383d6ea319ca1071698948e6059d99
zatikhair <zati.khair@gmail.com>
2021-08-07 23:29:43 +0800

    update

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,11 +1,15 @@
-const fibonacci = function (num) {
-  if (num < 0) {
-    return "OOPS";
-  } else if (num <= 1) {
-    return num;
-  } else {
-    return fibonacci(num - 1) + fibonacci(num - 2);
+const fibonacci = function (x) {
+  x = parseInt(x);
+  let fib = [1, 1];
+  let result = 0;
+  if (x < 1) return "OOPS";
+  if (x == 1) return 1;
+  if (x == 2) return 1;
+  for (i = 2; i < x; i++) {
+    result = fib[i - 1] + fib[i - 2];
+    fib.push(result);
   }
+  return result;
 };
 
 module.exports = fibonacci;
